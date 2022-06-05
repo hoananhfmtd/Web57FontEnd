b5214dbfda50f677597f775490f27f8bd28de13e
tuanlinh <tuanlinhtn2k1@gmail.com>
2022-06-05 15:45:42 +0700

    Tuan Linh

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Login from './pages/Login/Login';
 import Register from './pages/SignUp/Register';
+import Question from './pages/Question/Question';
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,7 @@ function App() {
         <Route path="" element={<div>List question</div>} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Register />} />
-        <Route path="question/:id" element={<div>Detail question</div>} />
+        <Route path="question/:id" element={<Question/>} />
       </Routes>
     </div>
   );
