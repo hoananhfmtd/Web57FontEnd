f6451e56d8726f48ce34dcb6a315ccbab86e54a7
tuanlinh <tuanlinhtn2k1@gmail.com>
2022-06-09 20:49:08 +0700

    aa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css';
-import Login from './pages/Login/Login';
-import Register from './pages/SignUp/Register';
-import Question from './pages/Question/Question';
+import './App.css'
+import Login from './pages/Login/Login'
+import Register from './pages/SignUp/Register'
+import Question from './pages/Question/Question'
+import React from 'react'
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="" element={<div>List question</div>} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Register />} />
-        <Route path="question/:id" element={<Question/>} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="" element={<div>List question</div>} />
+				<Route path="login" element={<Login />} />
+				<Route path="signup" element={<Register />} />
+				<Route path="question/:id" element={<Question />} />
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Question/Question.js
+++ src/pages/Question/Question.js
@@ -1,103 +1,367 @@
-import "./Question.css";
-import React from "react";
+import './Question.css'
+import React from 'react'
+import axios, { Axios } from 'axios'
+import { useParams } from 'react-router-dom'
 
 function Question() {
-  const navbarHandle = () => document.querySelector('.offcanvas-collapse').classList.toggle('open')
-  
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" aria-label="Main navigation">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Question & Anwser</a>
-            <button className="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation" onClick={()=>{navbarHandle()}}>
-              <span className="navbar-toggler-icon"></span>
-            </button>
-        
-            <div className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-  
-              <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
-        </nav>
-        
-        
-        <main className="container">
-          <div className="d-flex align-items-center  p-3 my-3 text-white bg-purple rounded shadow-sm">
-            {/* <img className="me-3" src="../assets/brand/bootstrap-logo-white.svg" alt="" width="48" height="38"></img> */}
-            <div className="lh-1">
-              <h1 className="tt1">TITLE</h1>
-              <small>Content</small>
-            </div>
-          </div>
-      
-        
-          <div className="my-3 p-3 bg-body rounded shadow-sm">
-            <h6 className="border-bottom pb-2 mb-0">Suggestions</h6>
-            <div className="d-flex text-muted pt-3">
-              <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-        
-              <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
-                <div className="d-flex justify-content-between">
-                  <strong className="text-gray-dark">Full Name</strong>
-                  <a href="#">date</a>
-                </div>
-                <span className="d-block">@username</span>
-              </div>
-            </div>
-            <div className="d-flex text-muted pt-3">
-              <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-        
-              <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
-                <div className="d-flex justify-content-between">
-                  <strong className="text-gray-dark">Full Name</strong>
-                  <a href="#">date</a>
-                </div>
-                <span className="d-block">@username</span>
-              </div>
-            </div>
-            <div className="d-flex text-muted pt-3">
-              <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-        
-              <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
-                <div className="d-flex justify-content-between">
-                  <strong className="text-gray-dark">Full Name</strong>
-                  <a href="#">date</a>
-                </div>
-                <span className="d-block">@username</span>
-              </div>
-            </div>
-            <div className="d-flex text-muted pt-3">
-              <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-        
-              <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
-                <div className="d-flex justify-content-between">
-                  <strong className="text-gray-dark">Full Name</strong>
-                  <a href="#">date</a>
-                </div>
-                <span className="d-block">@username</span>
-              </div>
-            </div>
-            <div className="d-flex text-muted pt-3">
-              <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-        
-              <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
-                <div className="d-flex justify-content-between">
-                  <strong className="text-gray-dark">Full Name</strong>
-                  <a href="#">date</a>
-                </div>
-                <span className="d-block">@username</span>
-              </div>
-            </div>
-          </div>
-        </main></>
-    );
-  }
+	const navbarHandle = () =>
+		document.querySelector('.offcanvas-collapse').classList.toggle('open')
+	let [answer, setAnswer] = React.useState(),
+		[keyword, setKeyword] = React.useState(''),
+		[description, setDescription] = React.useState('')
 
-  
+	let [questions, setQuestion] = React.useState()
+	const { id: questionId } = useParams()
 
-  
-  export default Question;
-  
+	const filterHandle = (e) => {
+		setKeyword(e.target.value)
+	}
+
+	// gọi questions
+	React.useEffect(() => {
+		const getData = async () => {
+			let dataQuestion = await axios.get(
+				'http://localhost:8080/api/questions/' + questionId
+			)
+			setQuestion(dataQuestion.data.data)
+			console.log(dataQuestion)
+			console.log('aksjssj')
+		}
+		getData()
+	}, [questionId])
+	// goi answer
+	React.useEffect(() => {
+		const getData = async () => {
+			let dataAnswer = await axios.get(
+				`http://localhost:8080/api/answers?questionId=${questionId}`
+			)
+			setAnswer(dataAnswer.data.data)
+			console.log(dataAnswer.data.data)
+			console.log('aksjssj')
+		}
+		getData()
+	}, [])
+	console.log(answer)
+
+	const postDate = async (e) => {
+		e.preventDefault()
+		await axios
+			.post(
+				`http://localhost:8080/api/answers?questionId=${questionId}`,
+				{
+					description,
+				}
+			)
+			.then((res) => {
+				const getData = async () => {
+					let NewDataAnswer = await axios.get(
+						`http://localhost:8080/api/answers?questionId=${questionId}`
+					)
+					setAnswer(NewDataAnswer.data.data)
+					console.log('aksjssj')
+				}
+				getData()
+			})
+			.catch((err) => console.log(err))
+	}
+
+	return (
+		<>
+			<nav
+				className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark"
+				aria-label="Main navigation"
+			>
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						Question & Anwser
+					</a>
+					<button
+						className="navbar-toggler p-0 border-0"
+						type="button"
+						id="navbarSideCollapse"
+						aria-label="Toggle navigation"
+						onClick={() => {
+							navbarHandle()
+						}}
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+
+					<div
+						className="navbar-collapse offcanvas-collapse"
+						id="navbarsExampleDefault"
+					>
+						<form className="d-flex" role="search">
+							<input
+								className="form-control me-2"
+								type="search"
+								placeholder="Search"
+								aria-label="Search"
+								onInput={(event) => {
+									filterHandle(event)
+								}}
+							/>
+							<button
+								className="btn btn-outline-success"
+								type="submit"
+							>
+								Search
+							</button>
+						</form>
+					</div>
+				</div>
+			</nav>
+			<div className="container1">
+				<div className="containerExtra">
+					<button type="button" className="btn btn-light">
+						Home
+					</button>
+					<button type="button" className="btn btn-primary">
+						Ask Question
+					</button>
+					{/* <button type="button" className="btn btn-primary">
+						About Us
+					</button> */}
+				</div>
+				<main className="container mid">
+					<div className="d-flex align-items-center question my-3 text-white bg-purple rounded shadow-sm">
+						{/* <img className="me-3" src="../assets/brand/bootstrap-logo-white.svg" alt="" width="48" height="38"></img> */}
+						<div className="lh-1">
+							<small>
+								<ul className="questionData">
+									{questions && (
+										<div>
+											<div className="titleSmall">
+												<h3>
+													<span>Titel</span>
+													<spam className="titelTQues">
+														{questions.title}
+													</spam>
+												</h3>
+											</div>
+
+											<span className="textQuestion">
+												Question :
+											</span>
+											<span>{questions.description}</span>
+										</div>
+									)}
+								</ul>
+							</small>
+						</div>
+					</div>
+
+					<div>
+						{answer &&
+							answer.map((item) => {
+								if (
+									item.description != undefined
+									// .toLowerCase()
+									// .includes(keyword.toLowerCase().trim())
+								)
+									return (
+										<div className="my-3  bg-body rounded shadow-sm">
+											<div className="d-flex text-muted pt-3">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="40"
+													height="40"
+													fill="currentColor"
+													className="bi bi-person-circle"
+													viewBox="0 0 16 16"
+												>
+													<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+													<path
+														fill-rule="evenodd"
+														d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+													/>
+												</svg>
+												<strong className="text-gray-dark">
+													Full Name
+												</strong>
+												<div className=" mb-0 answerApi small lh-sm border-bottom w-100">
+													<div className="d-flex justify-content-between"></div>
+													<span className="d-block">
+														{' '}
+														<div key={item.id}>
+															{/* description:{' '} */}
+															{item.description}
+														</div>
+													</span>
+													<a className="date">
+														vote {item.vote}
+													</a>
+												</div>
+											</div>
+										</div>
+									)
+							})}
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<button
+									className="btn btn-outline-secondary"
+									type="button"
+									onClick={postDate}
+								>
+									Anwser
+								</button>
+							</div>
+							<input
+								type="text"
+								className=" inputCre"
+								aria-describedby="basic-addon1"
+								value={description}
+								onChange={(e) => setDescription(e.target.value)}
+							></input>
+						</div>
+					</div>
+				</main>
+			
+			</div>
+
+			<link
+				href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+				rel="stylesheet"
+				id="bootstrap-css"
+			></link>
+			<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+			<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+			<section id="footer">
+				<div className="container">
+					<div className="row text-center text-xs-center text-sm-left text-md-left">
+						<div className="col-xs-12 col-sm-4 col-md-4">
+							<h5>Quick links</h5>
+							<ul className="list-unstyled quick-links">
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										Home
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										About
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										FAQ
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="col-xs-12 col-sm-4 col-md-4">
+							<h5>Quick links</h5>
+							<ul className="list-unstyled quick-links">
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										Home
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										About
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										FAQ
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="col-xs-12 col-sm-4 col-md-4">
+							<h5>Quick links</h5>
+							<ul className="list-unstyled quick-links">
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										Home
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										About
+									</a>
+								</li>
+								<li>
+									<a href="javascript:void();">
+										<i className="fa fa-angle-double-right"></i>
+										FAQ
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+							<ul className="list-unstyled list-inline social text-center">
+								<li className="list-inline-item">
+									<a href="javascript:void();">
+										<i className="fa fa-facebook"></i>
+									</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">
+										<i className="fa fa-twitter"></i>
+									</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">
+										<i className="fa fa-instagram"></i>
+									</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">
+										<i className="fa fa-google-plus"></i>
+									</a>
+								</li>
+								<li className="list-inline-item">
+									<a
+										href="javascript:void();"
+										target="_blank"
+									>
+										<i className="fa fa-envelope"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+							<p>
+								<u>
+									<a href="https://www.nationaltransaction.com/">
+										National Transaction Corporation
+									</a>
+								</u>{' '}
+								is a Registered MSP/ISO of Elavon, Inc. Georgia
+								[a wholly owned subsidiary of U.S. Bancorp,
+								Minneapolis, MN]
+							</p>
+							<p className="h6">
+								&copy All right Reversed.
+								<a
+									className="text-green ml-2"
+									href="https://www.sunlimetech.com"
+									target="_blank"
+								>
+									Sunlimetech
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	)
+}
+
+export default Question
